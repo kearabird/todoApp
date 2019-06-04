894ee4515e247bba1376695f6253835a2851c426
keara <kearabird@yahoo.com>
2019-06-04 11:44:02 -0400

    Updates

---
+++ precache-manifest.fce3302f43062435c4c4797b35ba048b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "820375b17865d056f7ee8791a4e3dc85",
+    "url": "/todoApp/index.html"
+  },
+  {
+    "revision": "564b2983a3f858d80dfc",
+    "url": "/todoApp/static/css/main.9c679a29.chunk.css"
+  },
+  {
+    "revision": "5694e1898ca4c92fd14d",
+    "url": "/todoApp/static/js/2.6408f43b.chunk.js"
+  },
+  {
+    "revision": "564b2983a3f858d80dfc",
+    "url": "/todoApp/static/js/main.be9d6dbe.chunk.js"
+  },
+  {
+    "revision": "7ffb180d2dcaa0b59a98",
+    "url": "/todoApp/static/js/runtime~main.1d50937c.js"
+  }
+]);
